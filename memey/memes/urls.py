c42f9f05a3aa80625f72ac33f600e0911a32a842
Daniel Kinahan <danielkinahan@uvica.ca>
2024-03-02 16:01:12 -0500

    Improved view and added add and edit functions

--- memey/memes/urls.py
+++ memey/memes/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import meme_list
+from .views import meme_list, meme_add, meme_edit
 
 urlpatterns = [
     path('', meme_list, name='meme_list'),
+    path('add/', meme_add, name='meme_add'),
+    path('edit/<int:pk>/', meme_edit, name='meme_edit'),
 ]
--- memey/memes/views.py
+++ memey/memes/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render, redirect
-from .models import Meme
 from .forms import MemeForm
+from .models import Meme
 
 
 def meme_list(request):
     memes = Meme.objects.all()
+    return render(request, 'memes/meme_list.html', {'memes': memes})
+
+
+def meme_add(request):
     if request.method == 'POST':
-        form = MemeForm(request.POST, request.FILES)
+        form = MemeForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('meme_list')
     else:
         form = MemeForm()
-    return render(request, 'memes/meme_list.html', {'memes': memes, 'form': form})
+    return render(request, 'memes/meme_form.html', {'form': form})
+
+
+def meme_edit(request, pk):
+    meme = Meme.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = MemeForm(request.POST, instance=meme)
+        if form.is_valid():
+            form.save()
+            return redirect('meme_list')
+    else:
+        form = MemeForm(instance=meme)
+    return render(request, 'memes/meme_form.html', {'form': form})
