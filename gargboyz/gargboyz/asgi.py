993a012c34b2ccf30afbdcf3c600b1c8ad135cd1
Daniel Kinahan <danielkinahan@uvica.ca>
2024-03-03 17:15:01 -0500

    renamed project to gargboyz

--- gargboyz/gargboyz/asgi.py
+++ gargboyz/gargboyz/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for memey project.
+ASGI config for gargboyz project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'memey.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gargboyz.settings')
 
 application = get_asgi_application()
