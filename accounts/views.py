36eac42bd799565c60bb9cd2eabad944b40a6fcb
Daniel Kinahan <danielkinahan@uvica.ca>
2024-03-06 13:32:21 -0500

    users are default inactive until approval, fixed issue with randomizer in chromium

--- accounts/views.py
+++ accounts/views.py
@@ -7,3 +7,8 @@ class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+    def form_valid(self, form):
+        # Set is_active to False by default
+        form.instance.is_active = False
+        return super().form_valid(form)
