9a462143507ff7f893397cf13f7cd2084a6280d9
Daniel Kinahan <danielkinahan@uvica.ca>
2024-03-06 15:57:57 -0500

    Added a view for updating all memes and fixed filefields for edits

--- memes/urls.py
+++ memes/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('create/', create, name='create'),
     path('create/multiple/', create_multiple, name='create_multiple'),
     path('update/<int:pk>/', update, name='update'),
+    path('update/all/', update_all, name='update_all'),
     path('api/create/', api_create, name='api_create'),
     path('api/read/', api_read, name='api_read'),
 ]
