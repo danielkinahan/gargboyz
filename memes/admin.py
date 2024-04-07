05292cb53978e1c2e0f6e1fc420b789c87d6de04
Daniel Kinahan <github@danielkinahan.ca>
2024-04-06 20:12:20 -0400

    Admin view improvements

--- memes/admin.py
+++ memes/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 from .models import Meme, Author, Rating, Comment
 
-# Register your models here.
+@admin.register(Meme)
+class MemeAdmin(admin.ModelAdmin):
+    list_filter = ('authors', 'season', 'subseason', 'average_rating', 'rating_count', 'comment_count', 'meme_type')
+    search_fields = ('voice_recording_transcript',)
 
-admin.site.register(Meme)
 admin.site.register(Author)
-admin.site.register(Rating)
-admin.site.register(Comment)
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'meme', 'rating')
+    list_filter = ('user', 'meme', 'rating')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('body', 'user', 'meme', 'created_on')
+    list_filter = ('created_on',)
+    search_fields = ('body',)
