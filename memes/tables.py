69e7c393999f870b46141a951f28eae2361a8e7f
Daniel Kinahan <github@danielkinahan.ca>
2024-04-12 15:54:36 -0400

    Added better mobile view fixes #22

--- memes/tables.py
+++ memes/tables.py
@@ -2,20 +2,32 @@ import django_tables2 as tables
 from .models import Meme
 
 class MemeTable(tables.Table):
-    number = tables.TemplateColumn(template_name='memes/columns/number.html', verbose_name='Number')
+    large_hide_attr = {
+        "td" : {'class': 'align-middle d-none d-lg-table-cell'},
+        "th" : {'class': 'd-none d-lg-table-cell'}
+    }
+    medium_hide_attr = {
+        "td" : {'class': 'align-middle d-none d-md-table-cell'},
+        "th" : {'class': 'd-none d-md-table-cell'}
+    }
+    small_hide_attr = {
+        "td" : {'class': 'align-middle d-none d-sm-table-cell'},
+        "th" : {'class': 'd-none d-sm-table-cell'}
+    }
+    number = tables.TemplateColumn(template_name='memes/columns/number.html', verbose_name='Number', attrs=small_hide_attr)
     # declared_number = tables.Column(verbose_name='Declared number')
     meme_path = tables.TemplateColumn(template_name='memes/columns/meme.html', verbose_name='Meme', orderable=False)
     # meme_thumbnail
     # meme_type
     # meme_created_at = tables.Column(verbose_name='Meme constructed')
     voice_recording_path = tables.TemplateColumn(template_name='memes/columns/idea.html', verbose_name='Idea', orderable=False)
-    voice_recording_created_at = tables.Column(verbose_name='Idea date')
-    voice_recording_transcript = tables.Column(verbose_name='Transcript', orderable=False)
-    authors = tables.Column(verbose_name='Authors')
-    season = tables.Column(verbose_name='Season')
-    subseason = tables.Column(verbose_name='Subseason/Template')
-    average_rating = tables.TemplateColumn(template_name='memes/columns/rating.html', verbose_name='Rating')
-    comment_count = tables.TemplateColumn(template_name='memes/columns/actions.html', verbose_name='Actions')
+    voice_recording_created_at = tables.Column(verbose_name='Idea date', attrs=large_hide_attr)
+    voice_recording_transcript = tables.Column(verbose_name='Transcript', orderable=False, attrs=large_hide_attr)
+    authors = tables.Column(verbose_name='Authors', attrs=medium_hide_attr)
+    season = tables.Column(verbose_name='Season', attrs=large_hide_attr)
+    subseason = tables.Column(verbose_name='Subseason/Template', attrs=large_hide_attr)
+    average_rating = tables.TemplateColumn(template_name='memes/columns/rating.html', verbose_name='Rating', attrs=small_hide_attr)
+    comment_count = tables.TemplateColumn(template_name='memes/columns/actions.html', verbose_name='Actions', attrs=medium_hide_attr)
 
     class Meta:
         model = Meme
